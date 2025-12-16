85baff06110516a8066b37877197abb91a291002
Liam Duret <115427960+Poyo1300@users.noreply.github.com>
2025-12-16 17:18:02 +0100

    remove out

--- out/build/CATCH2MYBELOVED/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/l.duret/Desktop/CMake_Exo/CATCH2MYBELOVED
-# Build directory: C:/Users/l.duret/Desktop/CMake_Exo/out/build/CATCH2MYBELOVED
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-include("C:/Users/l.duret/Desktop/CMake_Exo/out/build/CATCH2MYBELOVED/UnitTest-b12d07c_include.cmake")
--- out/build/CATCH2MYBELOVED/UnitTest-b12d07c_include.cmake
+++
@@ -1,5 +0,0 @@
-if(EXISTS "C:/Users/l.duret/Desktop/CMake_Exo/out/build/CATCH2MYBELOVED/UnitTest-b12d07c_tests.cmake")
-  include("C:/Users/l.duret/Desktop/CMake_Exo/out/build/CATCH2MYBELOVED/UnitTest-b12d07c_tests.cmake")
-else()
-  add_test(UnitTest_NOT_BUILT-b12d07c UnitTest_NOT_BUILT-b12d07c)
-endif()
